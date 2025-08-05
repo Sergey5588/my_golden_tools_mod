57e60fed670388f1295872dc440ae84e0cc4f95f
Sergey5588 <sergey271109@gmail.com>
2025-08-05 23:16:14 +0300

    init

---
+++ src/client/java/sergey5588/mgtm/MyGoldenToolsModClient.java
@@ -0,0 +1,10 @@
+package sergey5588.mgtm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyGoldenToolsModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
