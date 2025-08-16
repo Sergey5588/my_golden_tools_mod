ab9bae8db0191796b1198cf68363387cd8d647e3
Sergey5588 <sergey271109@gmail.com>
2025-08-16 13:15:41 +0300

    Altar now checks blocks around it

--- src/main/java/sergey5588/mgtm/custom/blocks/AltarCore.java
+++ src/main/java/sergey5588/mgtm/custom/blocks/AltarCore.java
@@ -6,23 +6,54 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import sergey5588.mgtm.ModBlockEntityTypes;
-import sergey5588.mgtm.ModBlocks;
+import sergey5588.mgtm.MyGoldenToolsMod;
 import sergey5588.mgtm.custom.blockEntities.AltarCoreBlockEntity;
 
 public class AltarCore extends BlockWithEntity {
     public AltarCore(Block.Settings settings) {
         super(settings);
     }
+    public static final BlockPos[] Positions = {
+            new BlockPos(0,-1,0),
+            new BlockPos(1,-1,0),
+            new BlockPos(1,-1,1),
+            new BlockPos(0,-1,1),
+            new BlockPos(-1,-1,1),
+            new BlockPos(1,-1,-1),
+            new BlockPos(0,-1,-1),
+            new BlockPos(-1,-1,-1),
+            new BlockPos(-1,-1,0),
+    };
+    public static final Block[] StructureBlocks = {
+            Blocks.LAPIS_BLOCK,
+            Blocks.GOLD_BLOCK,
+            Blocks.GOLD_BLOCK,
+            Blocks.GOLD_BLOCK,
+            Blocks.GOLD_BLOCK,
+            Blocks.GOLD_BLOCK,
+            Blocks.GOLD_BLOCK,
+            Blocks.GOLD_BLOCK,
+            Blocks.GOLD_BLOCK,
 
+
+    };
+    public boolean isValidStructure(World world,BlockPos pos) {
+        for(int i=0; i < Positions.length; ++i) {
+            if(!world.getBlockState(Positions[i].add(pos.getX(), pos.getY(), pos.getZ())).isOf(StructureBlocks[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     @Override
     protected MapCodec<? extends AltarCore> getCodec() {
         return createCodec(AltarCore::new);
@@ -46,9 +77,10 @@ public class AltarCore extends BlockWithEntity {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (!world.isClient) {
-;
-            //player.openHandledScreen(AltarCoreBlockEntity);
-            player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+            if(isValidStructure(world, pos))
+                player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+            else
+                player.sendMessage(Text.translatable("gui.my-golden-tools-mod.invalid_multiblock"), true);
         }
         return ActionResult.SUCCESS;
     }
