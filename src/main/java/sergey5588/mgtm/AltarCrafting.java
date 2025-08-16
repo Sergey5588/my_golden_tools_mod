61e5a73b5b059df17095cfc4881237215fc20b30
Sergey5588 <sergey271109@gmail.com>
2025-08-16 23:08:08 +0300

    Altar can now repair golden tools for free!

--- src/main/java/sergey5588/mgtm/AltarCrafting.java
+++ src/main/java/sergey5588/mgtm/AltarCrafting.java
@@ -3,16 +3,34 @@ package sergey5588.mgtm;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import sergey5588.mgtm.custom.blockEntities.AltarCoreBlockEntity;
 import sergey5588.mgtm.utils.AltarCoreCraftC2SPayload;
 
+import java.util.Arrays;
+
 public class AltarCrafting {
     private static final Item[] GOLDEN_TOOLS = {
-
+            Items.GOLDEN_PICKAXE,
+            Items.GOLDEN_AXE,
+            Items.GOLDEN_BOOTS,
+            Items.GOLDEN_CHESTPLATE,
+            Items.GOLDEN_HELMET,
+            Items.GOLDEN_HOE,
+            Items.GOLDEN_HORSE_ARMOR,
+            Items.GOLDEN_LEGGINGS,
+            Items.GOLDEN_SHOVEL,
+            Items.GOLDEN_SWORD
     };
 
-    private void process_crafting() {
+    public static void process_crafting(DefaultedList<ItemStack> inventory) {
+
+        if(Arrays.stream(GOLDEN_TOOLS).toList().contains(inventory.getFirst().getItem())) {
+            inventory.getFirst().setDamage(0);
+            return;
+        }
 
     }
     public static void initialize() {
@@ -20,7 +38,7 @@ public class AltarCrafting {
 
             BlockPos blockPos = new BlockPos((int)payload.pos().x,(int)payload.pos().y,(int)payload.pos().z);
             if(context.player().getWorld().getBlockEntity(blockPos) instanceof AltarCoreBlockEntity blockEntity) {
-                blockEntity.getItems().set(0, ItemStack.EMPTY);
+                process_crafting(blockEntity.getItems());
             }
         });
     }
